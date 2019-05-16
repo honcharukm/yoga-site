a4b5deeae3432483e2dc1d1a9f80d14e2dd8878a
Maksym <gontcharuk@gmail.com>
2019-05-16 08:02:10 +0200

    Шестой коммит

--- index.js
+++ index.js
@@ -212,4 +212,89 @@ window.addEventListener("DOMContentLoaded", function() {
             inputContact[i].value = "";
         }
     });
+
+    // slider 
+
+    let slideIndex = 1,
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+
+    showSlides(slideIndex);
+    
+    function showSlides(n) {
+        if (n > slides.length) slideIndex = 1;
+        if (n < 1) slideIndex = slides.length;
+
+        slides.forEach(item => item.style.display = "none");
+
+        dots.forEach(item => item.classList.remove('dot-active'));
+
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].classList.add('dot-active');
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlides(n) {
+        showSlides(slideIndex = n);
+    }
+
+    prev.addEventListener('click', function() {
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', function() {
+        plusSlides(1);
+    });
+
+    dotsWrap.addEventListener('click', function(event) {
+        for (let i = 0; i < dots.length + 1; i++) {
+            if(event.target.classList.contains('dot') && event.target == dots[i-1]) {
+                currentSlides(i);
+            }
+        }
+    });
+
+    // calc
+
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        restDays = document.querySelectorAll('.counter-block-input')[1], 
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum = 0,
+        total = 0;
+
+    totalValue.innerHTML = total;
+
+    persons.addEventListener('input', function() {
+        personsSum = +this.value;
+        calcCore();
+    });
+
+    restDays.addEventListener('input', function() {
+        daysSum = +this.value;
+        calcCore();
+    });
+
+    place.addEventListener('change', function() {
+        calcCore();
+    });
+
+    function calcCore() {
+        if (personsSum !== 0 && daysSum !== 0) {
+            total = (daysSum + personsSum) * 4000 * +place.value;
+        } else {
+            total = 0;
+        }
+
+        totalValue.innerHTML = total;
+    }
+
+// end DOMContentLoaded
 });
